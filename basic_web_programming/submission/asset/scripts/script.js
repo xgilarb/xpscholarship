827238b0a408389d0eb4023598286fd4191961ab
xpynim <xpynim@gmail.com>
2024-06-05 15:50:15 +0700

    pyn1445dzq28:updatescript.js

--- basic_web_programming/submission/asset/scripts/script.js
+++ basic_web_programming/submission/asset/scripts/script.js
@@ -1,19 +1,29 @@
-// pengaturan konten - huthama
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButtons = document.querySelectorAll('.toggle-content');
+  const toggleButtons = document.querySelectorAll('.toggle-content');
 
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const content = this.nextElementSibling;
-            if (content.style.display === 'none' || content.style.display === '') {
-                content.style.display = 'block';
-                this.textContent = 'Hide Article';
-            } else {
-                content.style.display = 'none';
-                this.textContent = 'Show Article';
-            }
-        });
+  toggleButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      const content = this.nextElementSibling;
+
+      // Tutup semua elemen konten artikel lainnya
+      document.querySelectorAll('.toggle-content').forEach(otherButton => {
+        if (otherButton !== this && otherButton.nextElementSibling) { // Periksa apakah itu bukan tombol saat ini
+          const otherContent = otherButton.nextElementSibling;
+          otherContent.style.display = 'none';
+          otherButton.textContent = 'Show Article';
+        }
+      });
+
+      // Alihkan konten artikel yang diklik
+      if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        this.textContent = 'Hide Article';
+      } else {
+        content.style.display = 'none';
+        this.textContent = 'Show Article';
+      }
     });
+  });
 });
 
 // pengaturan profile - author
@@ -24,11 +34,11 @@ aside.style.alignItems = 'center';
 
 const img = document.querySelector('aside img');
 img.style.borderRadius = '5%';
-img.style.Width = '48%';
+img.style.width = '48%';
 img.style.height = '175px';
 img.style.cursor = 'pointer';
 img.addEventListener('click', function() {
-    window.open('https://www.dicoding.com/users/xpynim/academies', '_blank');
+  window.open('https://www.dicoding.com/users/xpynim/academies', '_blank');
 });
 
 // pengaturan floating-up button

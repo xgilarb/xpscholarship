637add4f5d0c674beb284e83e9bd72aa422ace55
xpynim <xpynim@gmail.com>
2024-06-05 11:52:11 +0700

    pyn1445dzq28:updatesubmissionitems

--- basic_web_programming/submission/asset/scripts/script.js
+++ basic_web_programming/submission/asset/scripts/script.js
@@ -1,3 +1,4 @@
+// pengaturan konten - huthama
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButtons = document.querySelectorAll('.toggle-content');
 
@@ -6,11 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
             const content = this.nextElementSibling;
             if (content.style.display === 'none' || content.style.display === '') {
                 content.style.display = 'block';
-                this.textContent = 'hide';
+                this.textContent = 'Hide Article';
             } else {
                 content.style.display = 'none';
-                this.textContent = 'show again';
+                this.textContent = 'Show Article';
             }
         });
     });
+});
+
+// pengaturan profile - author
+const aside = document.querySelector('aside');
+aside.style.display = 'flex';
+aside.style.flexDirection = 'column';
+aside.style.alignItems = 'center';
+
+const img = document.querySelector('aside img');
+img.style.borderRadius = '5%';
+img.style.Width = '48%';
+img.style.height = '175px';
+img.style.cursor = 'pointer';
+img.addEventListener('click', function() {
+    window.open('https://www.dicoding.com/users/xpynim/academies', '_blank');
 });
